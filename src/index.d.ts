4034bff4d3de42fcf3925d9d53611f1a49103be4
AliRzaev <alirzaev1996@gmail.com>
2021-12-08 22:09:56 +0300

    Fix type definitions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,4 @@
-declare type ZipItem<T> = T extends [infer First, ...infer Rest] ? (First extends (infer V)[] ? [V, ...ZipItem<Rest>] : First) : T;
-declare function zip<T extends unknown[][]>(...arrays: T): ZipItem<T>[];
+declare module '@alirzaev/zip-js' {
+    type ZipItem<T> = T extends [infer First, ...infer Rest] ? (First extends (infer V)[] ? [V, ...ZipItem<Rest>] : First) : T;
+    function zip<T extends unknown[][]>(...arrays: T): ZipItem<T>[];
+}

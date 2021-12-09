b88a6a399df65e478fc0f7ab3d13a2fd57043fc5
AliRzaev <alirzaev1996@gmail.com>
2021-12-09 13:31:42 +0300

    Add the JSDoc annotations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 export type ZipItem<T> = T extends [infer First, ...infer Rest] ? (First extends (infer V)[] ? [V, ...ZipItem<Rest>] : First) : T;
 
+/**
+ * Creates an array of grouped elements, the first of which contains the
+ * first elements of the given arrays, the second of which contains the
+ * second elements of the given arrays, and so on.
+ * 
+ * @param {T} arrays The arrays to process.
+ * @returns {ZipItem<T>} Returns the new array of grouped elements.
+ * @example
+ *
+ * zip(['a', 'b'], [1, 2], [true, false]);
+ * // => [['a', 1, true], ['b', 2, false]]
+ */
 export function zip<T extends unknown[][]> (...arrays: T): ZipItem<T>[] {
     const minLength = Math.min(...(arrays.map(array => array.length)));
 
